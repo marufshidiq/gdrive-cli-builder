6aeeed8d2c7512f146a02a575314159cd0b74583
Ma'ruf Shidiq <maruf.shidiq@mail.ugm.ac.id>
2020-04-30 21:33:42 +0700

    Initialize v1 (#1)
    * Initialize readme
    
    * Initialize workflows

---
+++ build/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+APP_NAME="gdrive"
+
+BIN_PATH="_release/bin"
+
+PLATFORM=$1
+
+GOOS=${PLATFORM%/*}
+GOARCH=${PLATFORM#*/}
+BIN_NAME="${APP_NAME}-${GOOS/darwin/osx}-${GOARCH/amd64/x64}"
+
+if [ $GOOS == "windows" ]; then
+    BIN_NAME="${BIN_NAME}.exe"
+fi
+
+# Raspberrypi seems to need arm5 binaries
+if [ $GOARCH == "rpi" ]; then
+    export GOARM=5
+    GOARCH="arm"
+else
+    unset GOARM
+fi
+
+export GOOS=$GOOS
+export GOARCH=$GOARCH
+
+echo "Building $BIN_NAME"
+go build -ldflags '-w -s' -o ${BIN_PATH}/${BIN_NAME}
