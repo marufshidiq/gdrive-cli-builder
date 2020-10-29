3fd0fb33e2a2532551ad98e7233a26d68a5956e7
vanloc <vvanloc.vo@gmail.com>
2020-10-30 04:53:41 +0700

    add support mips and mipsle

--- build/build-selected.sh
+++ build/build-selected.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PLATFORMS=(darwin/386 darwin/amd64 darwin/arm darwin/arm64 dragonfly/amd64 freebsd/386 freebsd/amd64 freebsd/arm linux/386 linux/amd64 linux/arm linux/arm64 linux/ppc64 linux/ppc64le linux/mips64 linux/mips64le linux/rpi netbsd/386 netbsd/amd64 netbsd/arm openbsd/386 openbsd/amd64 openbsd/arm plan9/386 plan9/amd64 solaris/amd64 windows/386 windows/amd64)
+PLATFORMS=(darwin/386 darwin/amd64 darwin/arm darwin/arm64 dragonfly/amd64 freebsd/386 freebsd/amd64 freebsd/arm linux/386 linux/amd64 linux/arm linux/arm64 linux/ppc64 linux/ppc64le linux/mips linux/mipsle linux/mips64 linux/mips64le linux/rpi netbsd/386 netbsd/amd64 netbsd/arm openbsd/386 openbsd/amd64 openbsd/arm plan9/386 plan9/amd64 solaris/amd64 windows/386 windows/amd64)
 LIST="list.txt"
 BIN_PATH="_release/bin"
 mkdir $BIN_PATH
